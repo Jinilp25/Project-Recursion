e552b3c26a5085ef82b8cf8f3fe909b464b091c1
Jinilp25 <j.psg626@gmail.com>
2024-12-27 08:46:14 -0700

    Completed

--- main.rb
+++ main.rb
@@ -15,13 +15,36 @@ def fibs(n)
   arr
 end
 
-def fibs_rec(n, arr)
-  if n < 2
-    arr << n
+def fibs_rec(n)
+  if n < 1
+    return []
   else
-    
+    arr = fibs_rec(n - 1)
+    if arr.length < 2
+      arr << arr.length
+    else
+      arr << arr[-1] + arr[-2]
+    end
+  end
+  arr
 end
 
-p fibs_rec(3)
-# def merge_sort(arr)
+def merge_sort(arr)
+  new_arr = []
+  n = arr.length
+  return arr if n <= 1
 
+  first_half = merge_sort(arr[0, n / 2])
+  second_half = merge_sort(arr[n / 2, n])
+
+  until first_half.empty? || second_half.empty?
+    if first_half[0] < second_half[0]
+      new_arr << first_half.shift
+    else
+      new_arr << second_half.shift
+    end
+  end
+
+  new_arr += first_half + second_half
+  new_arr
+end
